b11293e7cb80979b0c028dc4bbb09a3dbe62a226
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-11 17:12:45 -0600

    added more code to omdb module

--- omdb-module.js
+++ omdb-module.js
@@ -12,21 +12,26 @@ for (var i = 2; i < nodeArgs.length; i++) {
 
 var queryURL = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy';
 axios.get(queryURL).then(
-  function (response) {
-    console.log('Movie: ' + response.data.Title),
-    console.log('\nRelease: ' + response.data.Year),
-    console.log('\nIMDB Rating: ' + response.data.imdbRating),
-    console.log('\nRotten Tomatoes Score: ' + response.data.Ratings[1].Value),
-    console.log('\nCountry: ' + response.data.Country),
-    console.log('\nLanguage: ' + response.data.Language),
-    console.log('\nPlot: ' + response.data.Plot),
-    console.log('\nActors: ' + response.data.Actors)
-  }
-)
+    function (response) {
+      console.log('Movie: ' + response.data.Title),
+        console.log('\nRelease: ' + response.data.Year),
+        console.log('\nIMDB Rating: ' + response.data.imdbRating),
+        console.log('\nRotten Tomatoes Score: ' + response.data.Ratings[1].Value),
+        console.log('\nCountry: ' + response.data.Country),
+        console.log('\nLanguage: ' + response.data.Language),
+        console.log('\nPlot: ' + response.data.Plot),
+        console.log('\nActors: ' + response.data.Actors)
+    }
+  )
   .catch(function (error) {
     if (error.response) {
       console.log('Data:  ' + error.response.data);
       console.log('\nStatus: ' + error.response.status);
       console.log('\nHeaders: ' + error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error: ' + error.message);
     }
-  })
+    console.log(error.config);
+  });
