3880245ebbd1361a0b317ad4da5e0a99b8f45809
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-11 17:11:07 -0600

    added some code to omdb-module

---
+++ omdb-module.js
@@ -0,0 +1,32 @@
+var axios = require('axios');
+var nodeArgs = process.argv;
+var movieName = '';
+
+for (var i = 2; i < nodeArgs.length; i++) {
+  if (i > 2 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  } else {
+    movieName += nodeArgs[i];
+  }
+}
+
+var queryURL = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy';
+axios.get(queryURL).then(
+  function (response) {
+    console.log('Movie: ' + response.data.Title),
+    console.log('\nRelease: ' + response.data.Year),
+    console.log('\nIMDB Rating: ' + response.data.imdbRating),
+    console.log('\nRotten Tomatoes Score: ' + response.data.Ratings[1].Value),
+    console.log('\nCountry: ' + response.data.Country),
+    console.log('\nLanguage: ' + response.data.Language),
+    console.log('\nPlot: ' + response.data.Plot),
+    console.log('\nActors: ' + response.data.Actors)
+  }
+)
+  .catch(function (error) {
+    if (error.response) {
+      console.log('Data:  ' + error.response.data);
+      console.log('\nStatus: ' + error.response.status);
+      console.log('\nHeaders: ' + error.response.headers);
+    }
+  })
