d7041576f65649f8890e7c9bfdd064313dcf03df
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-11 14:35:02 -0600

    some

--- keys.js
+++ keys.js
@@ -4,8 +4,9 @@ console.log('this is loaded');
 var spotify = function() {
     this.id = process.env.SPOTIFY_ID;
     this.secret = process.env.SPOTIFY_SECRET;
+    
 }
-var spot = new spotify();
-
+var spot = new spotify(){};
+console.log(this.secret);
 module.exports = spot;
 
