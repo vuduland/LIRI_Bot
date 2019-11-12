0b6c33204382f7f6b10221e1e99a20d4415e1585
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-11 18:48:31 -0600

    bands-module not working

--- bands-module.js
+++ bands-module.js
@@ -1,37 +1,37 @@
-var axios = require('axios');
-var moment = require('moment');
+var axios = require("axios");
+var moment = require("moment");
 var nodeArgs = process.argv;
-var songName = "";
+var artistName = " ";
 
-for (let i = 2; i < nodeArgs.length; i++) {
+for (var i = 2; i < nodeArgs.length; i++) {
   if (i > 2 && i < nodeArgs.length) {
-    songName = songName + '+' + nodeArgs[i];
+    artistName = artistName + "+" + nodeArgs[i];
   } else {
-    songName += nodeArgs[i];
+    artistName += nodeArgs[i];
   }
 }
 
-var queryURL = 'https://rest.bandsintown.com/artists/' + songName + '/events?app_id=codingbootcamp';
+var queryURL = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
 
 axios.get(queryURL).then(
     function (response) {
-      console.log('================================================'),
-        console.log('\nVenue: ' + response.data[0].venue.name),
-        console.log('\nLocale: ' + response.data[0].venue.city),
-        console.log('\nDate: ' + moment(response.data[0].datetime).format('L')),
-        console.log('================================================')
+      console.log("================================================"),
+        console.log("\nVenue: " + response.data[0].venue.name),
+        console.log("\nLocale: " + response.data[0].venue.city),
+        console.log("\nDate: " + moment(response.data[0].datetime).format("L")),
+        console.log("================================================")
     }
   )
   .catch(function (error) {
     if (error.response) {
-      console.log('\n================================================'),
-        console.log('Data: ' + error.response.data)
-      console.log('\n================================================'),
-        console.log('Status: ' + error.response.status);
-      console.log('\n================================================'),
-        console.log('Headers: ' + error.response.headers);
+      console.log("\n================================================"),
+        console.log("Data: " + error.response.data),
+        console.log("\n================================================"),
+        console.log("Status: " + error.response.status),
+        console.log("\n================================================"),
+        console.log("Headers: " + error.response.headers)
     } else if (error.request) {
-      console.log('Error: ', error.message);
+      console.log("Error: ", error.message);
     }
-    // console.log(error.config);
+    console.log(error.config);
   });
