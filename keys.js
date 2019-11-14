670134c91e11cee0410ee765ef4945779a8a9585
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-14 16:22:56 -0600

    forgot what I changed

--- keys.js
+++ keys.js
@@ -1,12 +1,12 @@
 console.log('this is loaded');
 
 
-var spotify = function() {
+var spotify = function () {
     this.id = process.env.SPOTIFY_ID;
     this.secret = process.env.SPOTIFY_SECRET;
-    
+
 }
-var spot = new spotify(){};
+var spot = new spotify(),{ };
 console.log(this.secret);
 module.exports = spot;
 
--- omdb-module.js
+++ omdb-module.js
@@ -12,20 +12,20 @@ for (var i = 2; i < nodeArgs.length; i++) {
 
 var queryURL = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy';
 axios.get(queryURL).then(
-    function (response) {
-      console.log('================================================'),
-        console.log('\nMovie: ' + response.data.Title),
-        console.log('\nRelease: ' + response.data.Year),
-        console.log('\nIMDB Rating: ' + response.data.imdbRating),
-        console.log('\nRotten Tomatoes Score: ' + response.data.Ratings[1].Value),
-        console.log('\nCountry: ' + response.data.Country),
-        console.log('\nLanguage: ' + response.data.Language),
-        console.log('\nPlot: ' + response.data.Plot),
-        console.log('\nActors: ' + response.data.Actors),
-        console.log('================================================')
+  function (response) {
+    console.log('================================================'),
+      console.log('\nMovie: ' + response.data.Title),
+      console.log('\nRelease: ' + response.data.Year),
+      console.log('\nIMDB Rating: ' + response.data.imdbRating),
+      console.log('\nRotten Tomatoes Score: ' + response.data.Ratings[1].Value),
+      console.log('\nCountry: ' + response.data.Country),
+      console.log('\nLanguage: ' + response.data.Language),
+      console.log('\nPlot: ' + response.data.Plot),
+      console.log('\nActors: ' + response.data.Actors),
+      console.log('================================================')
 
-    }
-  )
+  }
+)
   .catch(function (error) {
     if (error.response) {
       console.log('Data:  ' + error.response.data);
