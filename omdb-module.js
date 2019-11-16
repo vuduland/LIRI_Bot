6330e3de35a132a478e2aca50f128dca030c94a6
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-15 22:12:51 -0600

    EDITED OMDB

--- omdb-module.js
+++ omdb-module.js
@@ -1,48 +1,47 @@
-/* eslint-disable no-unused-expressions */
-const axios = require('axios');
+var axios = require('axios');
 
-const nodeArgs = process.argv;
-let movieName = '';
+var nodeArgs = process.argv;
+var movieName = '';
 
-// eslint-disable-next-line no-plusplus
-for (let i = 2; i < nodeArgs.length; i++) {
+for (var i = 2; i < nodeArgs.length; i++) {
   if (i > 2 && i < nodeArgs.length) {
     movieName = `${movieName}+${nodeArgs[i]}`;
   } else {
     movieName += nodeArgs[i];
   }
 }
-
-const queryURL = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`;
-axios
-  .get(queryURL)
-  .then(function(response) {
-    const resp = response;
-    // eslint-disable-next-line no-sequences
-    console.log('================================================'),
-      console.log(`\nMovie: ${resp.data.Title}`),
-      console.log(`\nRelease: ${resp.data.Year}`),
-      console.log(`\nIMDB Rating: ${resp.data.imdbRating}`),
-      console.log(`\nRotten Tomatoes Score: ${resp.data.Ratings[1].Value}`),
-      console.log(`\nCountry: ${resp.data.Country}`),
-      console.log(`\nLanguage: ${resp.data.Language}`),
-      console.log(`\nPlot: ${resp.data.Plot}`),
-      console.log(`\nActors: ${resp.data.Actors}`),
-      console.log('================================================');
-  })
-  .catch(function(error) {
-    if (error.response) {
-      console.log(`Data:  ${error.response.data}`);
-      console.log(`\nStatus: ${error.response.status}`);
-      console.log(`\nHeaders: ${error.response.headers}`);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log(
-        `\n================================================\nError: ${
+var OMDB = function (search) {
+  var queryURL = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`;
+  axios
+    .get(queryURL)
+    .then(function (response) {
+      var resp = response;
+      console.log('================================================'),
+        console.log(`\nMovie: ${resp.data.Title}`),
+        console.log(`\nRelease: ${resp.data.Year}`),
+        console.log(`\nIMDB Rating: ${resp.data.imdbRating}`),
+        console.log(`\nRotten Tomatoes Score: ${resp.data.Ratings[1].Value}`),
+        console.log(`\nCountry: ${resp.data.Country}`),
+        console.log(`\nLanguage: ${resp.data.Language}`),
+        console.log(`\nPlot: ${resp.data.Plot}`),
+        console.log(`\nActors: ${resp.data.Actors}`),
+        console.log('================================================');
+    })
+    .catch(function (error) {
+      if (error.response) {
+        console.log(`Data:  ${error.response.data}`);
+        console.log(`\nStatus: ${error.response.status}`);
+        console.log(`\nHeaders: ${error.response.headers}`);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log(
+          `\n================================================\nError: ${
           error.message
-        }`
-      );
-    }
-    // console.log(error.config);
-  });
+          }`
+        );
+      }
+      console.log(error.config);
+    });
+};
+module.exports = OMDB;
